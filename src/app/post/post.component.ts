de08f544f3183235e9490bc296440b99da79abcd
Nikola Bodrozic <nikola.bodrozic@example.com>
2018-01-29 16:36:55 +0100

    deep dive in JSON no map operator

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -6,21 +6,44 @@ import { Http} from '@angular/http';
   template: `
 <ul class="list-group">
   <li *ngFor="let post of posts" class="list-group-item">
-    {{ post.title }}
+    {{ post['body'].foo }}
   </li>
 </ul>
+{{ post}}
   `,
   styles: []
 })
+
+/*
+{
+  "files":[
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "sunt aut",
+      "body": {"foo":"bar"}
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "qui est esse",
+      "body": {"foo":"baz"}
+    }
+  ]
+}
+*/
 export class PostComponent implements OnInit {
 
   posts: any[];
   constructor(http: Http) { 
     http
-    .get('http://jsonplaceholder.typicode.com/posts')
-    .subscribe(resp => this.posts = resp.json());
+    .get('http://nikolabodr.com/i2.php')
+    .subscribe(resp => {
+      this.posts = resp.json()['files']
+      console.log(JSON.stringify(this.posts[0]['body'].foo));    
+      }
+    );
   }
-
   ngOnInit() {
   }
 
